de2ab391e81af4c7009bbad843933151e0fa3ee8
Wratten <daniel.wratten@gmail.com>
2022-03-19 10:44:08 +0800

    setup models, db and views files

---
+++ models/Blog.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../db/connect");
+const User = require("./User");
+
+class Blog extends Model {}
+
+Blog.init(
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        key: "id",
+        model: User,
+      },
+    },
+    createdAt: {
+      field: "created_at",
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      field: "updated_at",
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "blog",
+  }
+);
+
+module.exports = Blog;
---
+++ models/Comment.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../db/connect");
+const User = require("./User");
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        key: "id",
+        model: User,
+      },
+    },
+    blog_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        key: "id",
+        model: User,
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comment",
+  }
+);
+
+module.exports = Comment;
